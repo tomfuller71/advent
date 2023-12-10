e601414e0b02e160f584a8e3e9d0e9e25addc13f
tfBosLA35 <67017587+tomfuller71@users.noreply.github.com>
2023-12-10 11:03:56 -0500

    day 9 p1 test

--- 2023/Day9/day_9.py
+++ 2023/Day9/day_9.py
@@ -1,8 +1,16 @@
 from aoc_utils import day_parser
 
+def predict_recursive(seq):
+    if all(s==0 for s in seq):
+        return 0
+    diffs = [seq[i] - seq[i - 1] for i in range(1, len(seq))]
+    return predict_recursive(diffs)
+
 
 def main(data, log=False):
+    lines = [map(int, line.split()) for line in data.split("\n")]
     # Part 1
+    print(f"Part 1: {sum(predict(line) for line in lines)}")
 
 
     # Part 2
