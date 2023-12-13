644cc5f0f510d4bc6a3715888b00ebb4739e964d
tfBosLA35 <67017587+tomfuller71@users.noreply.github.com>
2023-12-13 21:07:31 +0000

    day11

---
+++ 2023/Day11/day_11.py
@@ -0,0 +1,20 @@
+from aoc_utils import day_parser
+
+
+def main(data, log=False):
+    # Part 1
+
+
+    # Part 2
+
+
+    return
+
+
+if __name__ == "__main__":
+    args = day_parser().parse_args()
+    with open(args.infile) as f:
+        print(f"Using data from {args.infile}.\n")
+        data = f.read().strip()
+    main(data, log=args.log)
+    main(data, log=args.log)
